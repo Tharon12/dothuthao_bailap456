a92bca0990dff4ba156ed256e319cfd7ce5f2314
Thảo Đỗ <do99thao@gmail.com>
2020-06-08 08:54:44 +0700

    Add project files.

---
+++ dothuthao_lap456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dothuthao_lap456.MvcApplication" Language="C#" %>
---
+++ dothuthao_lap456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dothuthao_lap456.Startup))]
+namespace dothuthao_lap456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
